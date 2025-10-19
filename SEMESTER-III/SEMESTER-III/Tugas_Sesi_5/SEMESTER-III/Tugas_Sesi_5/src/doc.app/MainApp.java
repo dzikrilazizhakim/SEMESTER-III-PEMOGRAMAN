62487896657cc550e0868ff1c1534e18219b827c
dzikrilazizhakim <dzikril.aziz_ti24@nusaputra.ac.id>
2025-10-20 06:14:50 +0700

    Add files via upload

--- SEMESTER-III/SEMESTER-III/Tugas_Sesi_5/SEMESTER-III/Tugas_Sesi_5/src/doc.app/MainApp.java
+++ SEMESTER-III/SEMESTER-III/Tugas_Sesi_5/SEMESTER-III/Tugas_Sesi_5/src/doc.app/MainApp.java
@@ -1,1 +1,11 @@
+package doc.app;
+import java.util.ServiceLoader;
+import doc.core.DocumentProcessor; 
 
+public class MainApp { 
+    public static void main(String[] args){ 
+        
+        ServiceLoader<DocumentProcessor> loader=ServiceLoader.load(DocumentProcessor.class); 
+        
+        for(DocumentProcessor p:loader){ 
+            System.out.println(p.getFormatName()); } } }
