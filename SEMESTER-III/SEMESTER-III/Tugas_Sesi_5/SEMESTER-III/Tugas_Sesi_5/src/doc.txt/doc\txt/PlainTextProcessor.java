d1821f3a74351e4f672771cb68e7a529214fef35
dzikrilazizhakim <dzikril.aziz_ti24@nusaputra.ac.id>
2025-10-20 06:41:20 +0700

    Visual Studio Code

--- SEMESTER-III/SEMESTER-III/Tugas_Sesi_5/SEMESTER-III/Tugas_Sesi_5/src/doc.txt/doc\txt/PlainTextProcessor.java
+++ SEMESTER-III/SEMESTER-III/Tugas_Sesi_5/SEMESTER-III/Tugas_Sesi_5/src/doc.txt/doc\txt/PlainTextProcessor.java
@@ -1,1 +1,6 @@
+package doc.txt; 
+import doc.core.DocumentProcessor; 
 
+public class PlainTextProcessor implements DocumentProcessor { 
+    public String process(String c){return c;} 
+    public String getFormatName(){return "Plain Text";} }
