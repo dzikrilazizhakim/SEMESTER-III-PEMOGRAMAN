bcac77855e4b78cf4defe9d4b5c99232e120b7ce
dzikrilazizhakim <dzikril.aziz_ti24@nusaputra.ac.id>
2025-10-20 06:36:53 +0700

    Visual Studio Code

--- SEMESTER-III/SEMESTER-III/Tugas_Sesi_5/SEMESTER-III/Tugas_Sesi_5/src/doc.html/doc\html/HtmlProcessor.java
+++ SEMESTER-III/SEMESTER-III/Tugas_Sesi_5/SEMESTER-III/Tugas_Sesi_5/src/doc.html/doc\html/HtmlProcessor.java
@@ -1,1 +1,7 @@
+package doc.html; 
+import doc.core.DocumentProcessor; 
 
+public class HtmlProcessor implements DocumentProcessor { 
+    
+    public String process(String c){ return "<html><body>"+c+"</body></html>";} 
+    public String getFormatName(){return "HTML";} }
