7fc258bdda9df6d246c052fbb67ebb41fc440d43
dzikrilazizhakim <dzikril.aziz_ti24@nusaputra.ac.id>
2025-10-20 08:04:01 +0700

    Visual Studio Code

--- Pemograman Berbasis Platform/Sesi 4/Tugas Sesi 4/main.js
+++ Pemograman Berbasis Platform/Sesi 4/Tugas Sesi 4/main.js
@@ -1,1 +1,15 @@
+const film = {
+    judul: "Interstellar",
+    tahunRilis: 2014,
+    sutradara: "Christopher Nolan",
+    genre: ["Sci-Fi", "Adventure", "Drama"],
 
+    tampilkanDetail: function() {
+
+        return `Judul: ${this.judul}, Tahun Rilis: ${this.tahunRilis}, Sutradara: ${this.sutradara}, Genre: ${this.genre.join(', ')}`;
+    }
+};
+
+console.log("Nilai properti sutradara: - main.js:13", film.sutradara);
+console.log("Nilai elemen kedua dalam properti genre: - main.js:14", film.genre[1]);
+console.log("Hasil pemanggilan method tampilkanDetail: - main.js:15", film.tampilkanDetail());
